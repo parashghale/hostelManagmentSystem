b7cbaaa5aadaa24559ec2d65e4742a3a5df48c97
parashghale <parash.ghale20@gmail.com>
2019-12-07 18:55:13 +0545

    singup form

---
+++ app/src/main/java/com/example/hostelmanagmentsystem/singupActivity.java
@@ -0,0 +1,15 @@
+package com.example.hostelmanagmentsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class singupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_singup);
+        getSupportActionBar().setTitle("SINGUP FORM");
+    }
+}
